8b84a58b2279a3e1ce96cdc3650ed93a1c8716d8
solarillion <solarillion.ta@gmail.com>
2019-10-20 20:26:32 +0530

    Updates

--- app.py
+++ app.py
@@ -2,9 +2,7 @@
 
 import os
 from flask import Flask, jsonify, request
-import sqlite3
 from slacker import Slacker
-from credentials.keys import *
 
 app = Flask(__name__)
 app.debug = True
@@ -12,10 +10,10 @@ app.debug = True
 @app.route("/", methods=["GET", "POST"])
 def index():
 	if request.method == "POST":
-		slack_key = keys["slack"]
-		slack = Slacker(keys["slack"])
+		slack_key = os.environ.get("SLACK_KEY")
+		slack = Slacker(slack_key)
 		slack.chat.post_message("#ta_group", "Meeting")
 		return jsonify({"success": True})
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="2252")
+	app.run()
