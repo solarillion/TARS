9be0c91283699470bd10c77387bdea7af0b314bc
solarillion <solarillion.server@gmail.com>
2019-10-22 18:53:48 +0530

    Slack Events challenge

--- app.py
+++ app.py
@@ -11,13 +11,20 @@ app.debug = True
 slack_key = os.environ.get("SLACK_KEY")
 slack = Slacker(slack_key)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-	if request.method == "POST":
-		requests.post(request.json["response_url"], json={"success": True})
-		return jsonify({"success": True})
-	if request.method == "GET":
-		return render_template("index.html")
+	return render_template("index.html")
+
+@app.route("/event", methods=["POST"])
+def event():
+	# requests.post(request.json["response_url"], json={"success": True})
+	# return jsonify({"success": True})
+	return jsonify({"challenge": request.json["challenge"]})
+
+@app.route("/interact", methods=["POST"])
+def interact():
+	requests.post(request.json["response_url"], json={"success": True})
+	return jsonify({"success": True})
 
 if __name__ == "__main__":
 	app.run()
