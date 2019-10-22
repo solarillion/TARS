602a134fbc05e730cb1f9819e7a554638978a744
nandahkrishna <nanda.harishankar@gmail.com>
2019-10-22 18:40:46 +0530

    Updates

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 # Authors: Nanda H Krishna (https://github.com/nandahkrishna), Abhijith Ragav (https://github.com/abhijithragav)
 
 import os
+import requests
 from flask import Flask, jsonify, render_template, request
 from slacker import Slacker
 
 app = Flask(__name__)
 app.debug = True
 
+slack_key = os.environ.get("SLACK_KEY")
+slack = Slacker(slack_key)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
 	if request.method == "POST":
-		slack_key = os.environ.get("SLACK_KEY")
-		slack = Slacker(slack_key)
-		slack.chat.post_message("#ta_group", "Meeting")
-		challenge = request.json.get("challenge")
-		return jsonify({"challenge": challenge})
+		requests.post(request.json["response_url"], json={"success": True})
+		return jsonify({"success": True})
 	if request.method == "GET":
 		return render_template("index.html")
 
