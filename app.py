893cc48d89fe63b0047fe02f37c87c7993aa8c50
solarillion <solarillion.server@gmail.com>
2019-10-23 00:44:53 +0530

    Testing

--- app.py
+++ app.py
@@ -28,16 +28,16 @@ def event_handler(payload):
 
 @app.route("/interact", methods=["POST"])
 def interact():
-	payload = request.get_json(force=True)
+	payload = request.get_data(as_text=True)
 	thread = threading.Thread(target=interact_handler, args=(payload,))
 	thread.start()
 	return "", 200
 
 def interact_handler(payload):
-	response_url = payload["response_url"]
-	slack.chat.post_message("UDD17R796", response_url)
-	action_id = payload["action"]["action_id"]
-	slack.chat.post_message("UDD17R796", action_id)
+	slack.chat.post_message("UDD17R796", "Start")
+	data = payload
+	slack.chat.post_message("UDD17R796", data)
+	slack.chat.post_message("UDD17R796", "End")
 
 if __name__ == "__main__":
 	app.run()
