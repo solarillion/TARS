2a79650dc7eaf6cbe1d66c791b96d669e0b11a7a
nandahkrishna <nanda.harishankar@gmail.com>
2019-10-24 00:53:52 +0530

    Testing

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def event():
 	return "", 200
 
 def event_handler(payload):
-	slack.chat.post_message("UDD17R769", payload)
+	slack.chat.post_message("UDD17R796", payload)
 
 @app.route("/interact", methods=["POST"])
 def interact():
