8540619a333bab6d67ae7c98d84c6cc1fd827a2d
nandahkrishna <nanda.harishankar@gmail.com>
2019-10-27 00:03:10 +0530

    Major updates

--- app.py
+++ app.py
@@ -8,127 +8,114 @@ import json
 import requests
 import threading
 from flask import Flask, jsonify, render_template, request
-from slacker import Slacker
+import slack
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 app.debug = True
 
-vineethv_id = os.environ.get("VINEETHV_ID")
 tars_admin = os.environ.get("TARS_ADMIN")
 tars_token = os.environ.get("TARS_TOKEN")
 tars_bot_id = os.environ.get("TARS_BOT_ID")
-tars = Slacker(tars_token)
+vineethv_id = os.environ.get("VINEETHV_ID")
 
-vineethv_im_request = tars.im.open("UDD17R796")
-vineethv_im_channel = vineethv_im_request.body["channel"]["id"]
+tars = slack.WebClient(token=tars_token)
+slack_events_adapter = SlackEventAdapter(tars_token, "/event", app)
 
-post_message_url = "https://slack.com/api/chat.postMessage"
-post_headers = {"Content-type": "application/json", "Authorization": "Bearer " + tars_token}
+vineethv_im_request = tars.im_open(user="UDD17R796")
+vineethv_im_channel = vineethv_im_request.data["channel"]["id"]
+
 response_headers = {"Content-type": "application/json"}
 
-office_hours_set = {
-	"datetime_today": None,
-	"last_sunday": None,
-	"slot_message_ts": 0,
-	"slot_days": "",
-	"slot_start": "",
-	"slot_start_val": "",
-	"slot_end": "",
-	"slot_end_val": "",
-	"office_hours_text": ""
-}
+office_hours_messages = []
+time_values = []
 
 @app.route("/", methods=["GET"])
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/event", methods=["POST"])
-def event():
-	payload = json.loads(request.get_data())
-	thread = threading.Thread(target=event_handler, args=(payload,))
-	thread.start()
-	return "", 200
+@slack_events_adapter.on("message.im")
+def im_event(event_data):
+    thread = threading.Thread(target=im_event_handler, args=(event_data,))
+    thread.start()
+    return "", 200
 
-def event_handler(payload):
-	try:
-		text = payload["event"]["text"].replace("@", "")
-		user = payload["event"]["user"]
-		channel = payload["event"]["channel"]
-		time = payload["event_time"]
-		message = text + "\nFrom " + user + " in " + channel + "."
-		# tars.chat.post_message(tars_admin, message)
-		text = text.lower()
-		if "schedule office hours" in text:
-			office_hours_event_handler()
-	except:
-		if payload["event"]["bot_id"] != tars_bot_id:
-			message = json.dumps(payload).replace("@", "")
-			tars.chat.post_message(tars_admin, message)
-
-def office_hours_event_handler():
-	message = json.load(open("messages/request_office_hours.json"))
-	message["channel"] = vineethv_im_channel
-	office_hours_set["datetime_today"] = date.today()
-	if office_hours_set["datetime_today"].weekday() == 6:
-		office_hours_set["last_sunday"] = office_hours_set["datetime_today"]
-	else:
-		office_hours_set["last_sunday"] = office_hours_set["datetime_today"] + relativedelta(weekday=SU(-1))
-	requests.post(post_message_url, headers=post_headers, json=message)
-
+def im_event_handler(event_data):
+    text = event_data["event"]["text"].replace("@", "").lower()
+    if "schedule office hours" in text:
+        schedule_office_hours_event_handler()
+            
+def schedule_office_hours_event_handler():
+    office_hours_messages.clear()
+    message = json.load(open("messages/request_office_hours.json"))
+    message["channel"] = vineethv_im_channel
+    tars.chat_postMessage(message)
+        
 @app.route("/interact", methods=["POST"])
 def interact():
-	payload = json.loads(request.form.get("payload"))
-	thread = threading.Thread(target=interact_handler, args=(payload,))
-	thread.start()
-	return "", 200
-
+    payload = json.loads(request.form.get("payload"))
+    thread = threading.Thread(target=interact_handler, args=(payload,))
+    thread.start()
+    return "", 200
+    
 def interact_handler(payload):
-	action_id = payload["actions"][0]["action_id"]
-	if "office_hours" in action_id:
-		office_hours_interact_handler(action_id, payload)
+    action_id = payload["actions"][0]["action_id"]
+    if "office_hours" in action_id:
+        office_hours_interact_handler(action_id, payload)
 
 def office_hours_interact_handler(action_id, payload):
-	message = None
-	if action_id == "enter_office_hours":
-		message = json.load(open("messages/slot_office_hours.json"))
-		office_hours_set["slot_message_ts"] = tars.chat.post_message(vineethv_im_channel, "Your slot details will be updated here.").body["ts"]
-	elif action_id == "cancel_office_hours":
-		message = json.load(open("messages/cancel_office_hours.json"))
-	elif action_id == "select_days_office_hours":
-		options = payload["actions"][0]["selected_options"]
-		office_hours_set["slot_days"] = ""
-		for option in options:
-			office_hours_set["slot_days"] += option["text"]["text"] + " "
-		slot_text = office_hours_set["slot_days"] + office_hours_set["slot_start"] + office_hours_set["slot_end"]
-		office_hours_set["slot_message_ts"] = tars.chat.update(as_user=True, channel=vineethv_im_channel, ts=office_hours_set["slot_message_ts"], text=slot_text).body["ts"]
-	elif action_id == "select_start_time_office_hours":
-		office_hours_set["slot_start"] = ""
-		office_hours_set["slot_start"] = payload["actions"][0]["selected_option"]["text"]["text"] + " - "
-		office_hours_set["slot_start_val"] = payload["actions"][0]["selected_option"]["value"]
-		slot_text = office_hours_set["slot_days"] + office_hours_set["slot_start"] + office_hours_set["slot_end"]
-		office_hours_set["slot_message_ts"] = tars.chat.update(as_user=True, channel=vineethv_im_channel, ts=office_hours_set["slot_message_ts"], text=slot_text).body["ts"]
-	elif action_id == "select_end_time_office_hours":
-		office_hours_set["slot_end"] = ""
-		office_hours_set["slot_end"] = payload["actions"][0]["selected_option"]["text"]["text"] + "\n"
-		office_hours_set["slot_end_val"] = payload["actions"][0]["selected_option"]["value"]
-		slot_text = office_hours_set["slot_days"] + office_hours_set["slot_start"] + office_hours_set["slot_end"]
-		office_hours_set["slot_message_ts"] = tars.chat.update(as_user=True, channel=vineethv_im_channel, ts=office_hours_set["slot_message_ts"], text=slot_text).body["ts"]
-	elif action_id == "slot_done_office_hours":
-		if datetime.strptime(office_hours_set["slot_start_val"], "%H:%M").time() < datetime.strptime(office_hours_set["slot_end_val"], "%H:%M").time():
-			message = json.load(open("messages/confirm_office_hours.json"))
-			office_hours_set["office_hours_text"] = office_hours_set["slot_days"] + office_hours_set["slot_start"] + office_hours_set["slot_end"]			
-			office_hours_set["slot_days"] = ""
-			office_hours_set["slot_start"] = ""
-			office_hours_set["slot_end"] = ""
-		else:
-			slot_text = office_hours_set["slot_days"] + office_hours_set["slot_start"] + office_hours_set["slot_end"] + "Invalid!"
-			office_hours_set["slot_message_ts"] = tars.chat.update(as_user=True, channel=vineethv_im_channel, ts=office_hours_set["slot_message_ts"], text=slot_text).body["ts"]
-	elif action_id == "done_office_hours":
-		message = json.load(open("messages/done_office_hours.json"))
-		tars.chat.post_message(tars_admin, office_hours_set["office_hours_text"])
-	if message is not None:
-		response_url = payload["response_url"]
-		requests.post(response_url, headers=response_headers, json=message)
+    message = None
+    if action_id == "enter_office_hours":
+        text = "Your slot details will be updated here."
+        office_hours_messages.insert(0, tars.chat_postMessage(channel=vineethv_im_channel, text=text).data)
+        message = json.load(open("messages/slot_office_hours.json"))
+    elif action_id == "cancel_office_hours":
+        message = json.load(open("messages/cancel_office_hours.json"))
+    elif action_id == "select_days_office_hours":
+        options = payload["actions"][0]["selected_options"]
+        text = ""
+        for option in options:
+            text += option["text"]["text"] + " "
+        ts = office_hours_messages.pop(0)["ts"]
+        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
+    elif action_id == "select_start_time_office_hours":
+        time_values.clear()
+        time_values.append(payload["actions"][0]["selected_option"]["value"])
+        time_values.append("")
+        start = payload["actions"][0]["selected_option"]["text"]["text"] + " - "
+        details = office_hours_messages.pop(0)
+        ts = details["ts"]
+        text = details["message"]["text"]
+        text += start
+        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
+    elif action_id == "select_end_time_office_hours":
+        time_values.pop(1)
+        time_values.insert(1, payload["actions"][0]["selected_option"]["value"])
+        end = payload["actions"][0]["selected_option"]["text"]["text"] + "\n"
+        details = office_hours_messages.pop(0)
+        ts = details["ts"]
+        text = details["text"]
+        text += end
+        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
+    elif action_id == "slot_done_office_hours":
+        if datetime.strptime(time_values[0], "%H:%M").time() > datetime.strptime(time_values[1], "%H:%M").time():
+            message = json.load(open("messages/confirm_office_hours.json"))
+        else:
+            details = office_hours_messages.pop(0)
+            ts = details["ts"]
+            text = details["text"]
+            text += "Invalid!"
+            office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
+    elif action_id == "done_office_hours":
+        message = json.load(open("messages/done_office_hours.json"))
+        text = ""
+        for m in office_hours_messages:
+            text += m["message"]["text"]
+        tars.chat_postMessage(channel=tars_admin, text=text)
+    if message is not None:
+        message["channel"] = vineethv_im_channel
+        response_url = payload["response_url"]
+        requests.post(response_url, headers=response_headers, json=message)
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
