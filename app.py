dad7c36b7a907c0609294eed9ecfd95891cfcf15
solarillion <solarillion.server@gmail.com>
2019-11-29 16:50:37 +0530

    Rewrote TARS Finalised office hours code

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import threading
 from flask import Flask, jsonify, render_template, request
 import slack
 from slackeventsapi import SlackEventAdapter
+import pyrebase
 
 app = Flask(__name__)
 app.debug = True
@@ -18,18 +19,23 @@ tars_admin = os.environ.get("TARS_ADMIN")
 tars_token = os.environ.get("TARS_TOKEN")
 tars_secret = os.environ.get("TARS_SECRET")
 tars_bot_id = os.environ.get("TARS_BOT_ID")
+general_id = os.environ.get("GENERAL_ID")
 vineethv_id = os.environ.get("VINEETHV_ID")
+firebase_api_key = os.environ.get("FIREBASE_API_KEY")
 
 tars = slack.WebClient(token=tars_token)
 slack_events_adapter = SlackEventAdapter(tars_secret, "/event", app)
 
-vineethv_im_request = tars.im_open(user="UDD17R796")
+vineethv_im_request = tars.im_open(user=vineethv_id)
 vineethv_im_channel = vineethv_im_request.data["channel"]["id"]
 
-response_headers = {"Content-type": "application/json"}
-
-office_hours_messages = []
-time_values = []
+config = {
+  "apiKey": firebase_api_key,
+  "authDomain": "sf-tars.firebaseapp.com",
+  "databaseURL": "https://sf-tars.firebaseio.com/",
+  "storageBucket": "sf-tars.appspot.com"
+}
+firebase = pyrebase.initialize_app(config)
 
 @app.route("/", methods=["GET"])
 def index():
@@ -43,80 +49,35 @@ def message(event_data):
 
 def im_event_handler(event_data):
     text = event_data["event"]["text"].lower()
-    if "schedule office hours" in text:
-        schedule_office_hours_event_handler()
-            
-def schedule_office_hours_event_handler():
-    office_hours_messages.clear()
-    message = json.load(open("messages/request_office_hours.json"))
-    tars.chat_postMessage(channel=vineethv_im_channel, text=message["text"], blocks=message["blocks"])
-        
-@app.route("/interact", methods=["POST"])
-def interact():
-    payload = json.loads(request.form.get("payload"))
-    thread = threading.Thread(target=interact_handler, args=(payload,))
-    thread.start()
-    return "", 200
-    
-def interact_handler(payload):
-    action_id = payload["actions"][0]["action_id"]
-    if "office_hours" in action_id:
-        office_hours_interact_handler(action_id, payload)
+    if "request office hours" in text:
+        tars.chat_postMessage(channel=vineethv_im_channel, text="Sir, please fill your office hours in this form: https://forms.gle/eMoayTXg5KJCata68")
+    if "post office hours" in text:
+        db = firebase.database()
+        data = db.get().val()
+        data = data[list(data.keys())[0]]	
+        message = "Sir's office hours for the week:\n"
+        for item in data[1:]:
+            item["start"] = reformat_time(item["start"])
+            item["end"] = reformat_time(item["end"])
+            message += item["days"] + ": " + item["start"] + " - " + item["end"] + "\n"    
+        tars.chat_postMessage(channel=general_id, text=message)
 
-def office_hours_interact_handler(action_id, payload):
-    message = None
-    if action_id == "enter_office_hours":
-        text = "Your slot details will be updated here."
-        office_hours_messages.insert(0, tars.chat_postMessage(channel=vineethv_im_channel, text=text).data)
-        message = json.load(open("messages/slot_office_hours.json"))
-    elif action_id == "cancel_office_hours":
-        message = json.load(open("messages/cancel_office_hours.json"))
-    elif action_id == "select_days_office_hours":
-        options = payload["actions"][0]["selected_options"]
-        text = ""
-        for option in options:
-            text += option["text"]["text"] + " "
-        details = office_hours_messages.pop(0)
-        ts = details["ts"]
-        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
-    elif action_id == "select_start_time_office_hours":
-        time_values.clear()
-        time_values.append(payload["actions"][0]["selected_option"]["value"])
-        time_values.append("")
-        start = payload["actions"][0]["selected_option"]["text"]["text"] + " - "
-        details = office_hours_messages.pop(0)
-        ts = details["ts"]
-        text = details["message"]["text"]
-        text += start
-        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
-    elif action_id == "select_end_time_office_hours":
-        time_values.pop(1)
-        time_values.insert(1, payload["actions"][0]["selected_option"]["value"])
-        end = payload["actions"][0]["selected_option"]["text"]["text"] + "\n"
-        details = office_hours_messages.pop(0)
-        ts = details["ts"]
-        text = details["text"]
-        text += end
-        office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
-    elif action_id == "slot_done_office_hours":
-        if datetime.strptime(time_values[0], "%H:%M").time() > datetime.strptime(time_values[1], "%H:%M").time():
-            message = json.load(open("messages/confirm_office_hours.json"))
-        else:
-            details = office_hours_messages.pop(0)
-            ts = details["ts"]
-            text = details["text"]
-            text += "Invalid!"
-            office_hours_messages.insert(0, tars.chat_update(as_user=True, channel=vineethv_im_channel, ts=ts, text=text).data)
-    elif action_id == "done_office_hours":
-        message = json.load(open("messages/done_office_hours.json"))
-        text = ""
-        for m in office_hours_messages:
-            text += m["message"]["text"]
-        tars.chat_postMessage(channel=tars_admin, text=text)
-    if message is not None:
-        message["channel"] = vineethv_im_channel
-        response_url = payload["response_url"]
-        requests.post(response_url, headers=response_headers, json=message)
+def reformat_time(ts):
+    hour = int(ts.split(":")[0][-2:]) + 5
+    min = int(ts.split(":")[1][:2]) + 22
+    if min >= 60:
+        min -= 60
+        hour += 1
+    time = ""
+    if len(str(min)) == 1:
+        min = "0" + str(min)
+    if hour >= 12:
+        if hour != 12:
+            hour -= 12
+        time = str(hour) + ":" + str(min) + " PM"
+    else:
+        time = str(hour) + ":" + str(min) + " AM"
+    return time
 
 if __name__ == "__main__":
     app.run()
---
+++ scripts/Code.gs
@@ -0,0 +1,44 @@
+// Google Apps Script for handling Office Hours
+// Container: Office Hours Google Form
+// Replace SPREADSHEET_ID and FIREBASE_URL with the Office Hours (Responses) sheet ID and the TARS Firebase URL respectively
+
+// Function to clear form, sheet and database
+// Trigger: Time-based, Weekly, Saturday 6-7 pm
+function clearFunction() {
+  var form = FormApp.getActiveForm();
+  form.deleteAllResponses();
+  var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+  spreadsheet.deleteRows(2, 10);
+  var firebaseUrl = FIREBASE_URL;
+  var options = {
+    'method' : 'delete'
+  };
+  UrlFetchApp.fetch(firebaseUrl, options);
+}
+
+// Function to clear and overwrite database with new set of office hours
+// Trigger: Form-based, On submit
+function writeDataToFirebase() {
+  var firebaseUrl = FIREBASE_URL;
+  var options = {
+    'method' : 'delete'
+  };
+  UrlFetchApp.fetch(firebaseUrl, options);
+  var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+  var sheet = spreadsheet.getSheets()[0];
+  var data = sheet.getDataRange().getValues();
+  var dataToImport = {};
+  for(var i = 1; i < data.length; i++) {
+    dataToImport[i] = {
+      days:data[i][1],
+      start:data[i][2],
+      end:data[i][3]
+    };
+  }
+  var options = {
+    'method' : 'post',
+    'contentType': 'application/json',
+    'payload' : JSON.stringify(dataToImport)
+  };
+  UrlFetchApp.fetch(firebaseUrl, options);
+}
