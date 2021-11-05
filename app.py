ae92213b8835f7d32b0f4b88532b27c9559a0e8b
PrasannaKumaran <prasannakumaran176@gmail.com>
2021-11-05 13:35:49 +0530

    bug fix

--- app.py
+++ app.py
@@ -212,17 +212,17 @@ def show_meeting(message, say):
                 if count == 1: 
                     say("List of meetings booked by you : ")
           #      meetings.pop(meet)s
-        print(meeting_info)
         say(meeting_info)
         invites = 0
+        meeting_info = ""
         for meet in meetings.keys():
             if (slack_id in meetings[meet]["people"]):
                 invites += 1
                 item = db.child(key_fb_tars).child("meetings").child(meet).get().val()
-                meeting_info = f'{meet.split("_")[1]} : {item["desc"]}, {reformat_time(item["start"])} {reformat_time(item["start"])}-{reformat_time(item["end"])}'
+                meeting_info += f'{meet.split("_")[1]} : {item["desc"]}, {reformat_time(item["start"])} {reformat_time(item["start"])}-{reformat_time(item["end"])}\n'
                 if invites == 1:
                     say("List of meetings you've been invited to : ")
-                say(meeting_info)
+        say(meeting_info)
         #        meetings.pop(meet)
         if count == 0 and invites == 0:
             say("You have no upcoming meetings!.")
