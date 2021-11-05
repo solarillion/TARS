ffc67c41ba329b0a761ac1d72f0c07748b24ac18
PrasannaKumaran <prasannakumaran176@gmail.com>
2021-11-05 22:14:15 +0530

    bug fix

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def book_meeting(message, say):
         attendees = list(map(lambda x: app.client.users_info(user=x).data["user"]["profile"]["email"], attendees))
         people = people + attendees
         db.child(key_fb_tars).child("bookings").child(id).set({"meeting": meeting_description, "people": people, "people_slack": people_slack})
-        say("The meeting has been booked!")
+    say("The meeting has been booked!")
     
 @app.message("show meeting")
 def show_meeting(message, say):
@@ -212,18 +212,18 @@ def show_meeting(message, say):
                 if count == 1: 
                     say("List of meetings booked by you : ")
                 meetings.pop(meet)
-        say(meeting_info)
+                say(meeting_info)
         invites = 0
         meeting_info = ""
         for meet in meetings.keys():
             if (slack_id in meetings[meet]["people"]):
                 invites += 1
                 item = db.child(key_fb_tars).child("meetings").child(meet).get().val()
-                meeting_info += f'{meet.split("_")[1]} : {item["desc"]}, {reformat_time(item["start"])}-{reformat_time(item["end"])}\n'
+                meeting_info += f'{meet.split("_")[1]} : {item["2desc"]}, {reformat_time(item["start"])}-{reformat_time(item["end"])}\n'
                 if invites == 1:
                     say("List of meetings you've been invited to : ")
                 meetings.pop(meet)
-        say(meeting_info)
+                say(meeting_info)
         if count == 0 and invites == 0:
             say("You have no upcoming meetings!.")
         
